9b3166f78c32a4b5148186f1652d21704b75210b
IPolonio <Polonio.isaac@gmail.com>
2024-04-01 22:38:49 -0400

    Added feature

--- Libreria/Principal.php
+++ Libreria/Principal.php
@@ -4,7 +4,7 @@ session_start();
 
 require('class.php');
 include ("funciones.php");
-
+define('DATA_PATH','C:\xampp\htdocs\Proyecto-p3');
 define('PWD_SALT','OIDJFOIODFONE');
 define('SERVER_URL','http://localhost/Proyecto-P3');
 
--- personal.php
+++ personal.php
@@ -30,7 +30,7 @@
 <body>
     <div class="container">
         <h1 class="mt-5">Estado de Presencia <strong>: Maria</strong></h1>
-        <form action="submit_presence.php" method="post"  onsubmit="return showConfirmation()">
+        <form action="personal.php" method="post"  onsubmit="return showConfirmation()">
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="presence" id="present" value="present" onclick="hideJustification()">
                 <label class="form-check-label" for="present">
